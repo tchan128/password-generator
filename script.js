70b942b1ae13633303d17649a4a601a3bbab9eaa
Tiff Chan <tiffchan@Tiffanys-MacBook-Air.local>
2023-10-18 01:03:12 -0400

    Added prompt and restrictions for length of password

--- script.js
+++ script.js
@@ -12,3 +12,21 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Setting constants for min and max length of password
+
+MIN_LENGTH = 0;
+MAX_LENGTH = 128;
+
+// Prompting for length of password 
+
+var passwordLength = prompt("Please indicate length of password (min: 0, max:128)");
+
+if (passwordLength < MIN_LENGTH || passwordLength > MAX_LENGTH) {
+  alert("Please enter a number between 0 and 128.");
+  // Prompt password length again
+} else if (passwordLength.includes(".")) {
+  alert("Please enter a whole number.")
+  // Prompt password length again
+} // Condition to check if letters are in it - have yet to figure out how to code this.
+
