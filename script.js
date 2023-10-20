ab8ede3a50e5bf0f063ebec1d678d65958a7b598
Tiff Chan <tiffchan@Tiffanys-MacBook-Air.local>
2023-10-19 22:24:07 -0400

    added event listerner for when button is clicked

--- script.js
+++ script.js
@@ -20,43 +20,47 @@ MAX_LENGTH = 128;
 
 // Prompting for length of password 
 
-var passwordLength = prompt("Please indicate length of password (min: 0, max:128)");
+generateBtn.addEventListener('click', function() {
+  var passwordLength = prompt("Please indicate length of password (min: 0, max:128)");
 
-if (passwordLength < MIN_LENGTH || passwordLength > MAX_LENGTH) {
-  alert("Please enter a number between 0 and 128.");
-  // Prompt password length again
-} else if (passwordLength.includes(".")) {
-  alert("Please enter a whole number.")
-  // Prompt password length again
-} // Condition to check if letters are in it - have yet to figure out how to code this.
+  if (passwordLength < MIN_LENGTH || passwordLength > MAX_LENGTH) {
+    alert("Please enter a number between 0 and 128.");
+    // Prompt password length again
+  } else if (passwordLength.includes(".")) {
+    alert("Please enter a whole number.")
+    // Prompt password length again
+  } // Condition to check if letters are in it - have yet to figure out how to code this.
+  
+  // Prompting for character requirements 
+  
+  var lowercase = prompt("Would you like to include lowercase letters in your password? Enter: Y or N");
+  var uppercase = prompt("Would you like to include uppercase letters in your password? Enter: Y or N");
+  var numeric = prompt("Would you like to include numbers in your password? Enter: Y or N")
+  var specialLetters = prompt("Would you like to include special letters in your password? Enter: Y or N")
+  
+  // Try to make Yes = True, No = False
+  
+  function responseTranslator(response) {
+    if (response === "Yes" || response === "Y") {
+      response = true;
+      console.log("test 1");
+    } else {
+      response = false;
+      console.log("test 2");
+    }
+  }
+  
+  if (lowercase === "N" || uppercase === "N" || numeric === "N" || specialLetters === "N" ) {
+    alert("At least one of the characters has to be included.")
+  }
+  
+  // Generate random number form 0-9
+  // Generate random alphabet from a-z (both lower & uppercase)
+  // Generate random special letters 
+  
+  //Generate randomly from that list
+  
+  // While loop?
 
-// Prompting for character requirements 
+})
 
-var lowercase = prompt("Would you like to include lowercase letters in your password? Enter: Y or N");
-var uppercase = prompt("Would you like to include uppercase letters in your password? Enter: Y or N");
-var numeric = prompt("Would you like to include numbers in your password? Enter: Y or N")
-var specialLetters = prompt("Would you like to include special letters in your password? Enter: Y or N")
-
-// Try to make Yes = True, No = False
-
-// function responseTranslator(response) {
-//   if (response === "Yes" || response === "Y") {
-//     response = true;
-//     console.log("test 1");
-//   } else {
-//     response = false;
-//     console.log("test 2");
-//   }
-// }
-
-// var test = responseTranslator(hasLowercase);
-
-// console.log(hasLowercase);
-// console.log(test);
-// console.log(responseTranslator(hasUppercase));
-// console.log(responseTranslator(hasNumeric));
-// console.log(responseTranslator(hasSpecialLetters));
-
-if (lowercase === "N" || uppercase === "N" || numeric === "N" || specialLetters === "N" ) {
-  alert("At least one of the characters has to be included.")
-}
